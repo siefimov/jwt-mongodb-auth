adad7eb393f5f9e5f7ef30d04ac18eb54c73645e
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-10-31 18:13:20 +0200

    added db settings and basic middleware

--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cokiePaser = require('cookie-parser');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL);
         app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
     } catch (error) {
         console.log(error);
