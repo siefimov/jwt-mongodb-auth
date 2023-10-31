c73d1875007f282a94012b4a22d1aeefc1990cad
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-10-31 22:01:29 +0200

    added User controller service and routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const cokiePaser = require('cookie-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const router = require('./router/index');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
     try {
---
+++ server/router/index.js
@@ -0,0 +1,13 @@
+const Router = require('express').Router;
+const userController = require('../controllers/user.controller.js')
+
+const router = new Router();
+
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activation);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
+
+module.exports = router;
