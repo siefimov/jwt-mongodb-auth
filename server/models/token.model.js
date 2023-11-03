16670714dcbe2ef36c1018c45a4811e06d1cc1d0
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-03 13:06:01 +0200

    server: * import to require

--- server/models/token.model.js
+++ server/models/token.model.js
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose';
+const { Schema, model } = require('mongoose');
 
 const TokenSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
