48101426f4a29410ff5f22071e6b3a383815c64c
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-03 23:09:28 +0200

    server: * refresh function from userService

--- server/service/user.service.js
+++ server/service/user.service.js
@@ -65,6 +65,17 @@ class UserService {
         if (!refreshToken) {
             throw ApiError.UnauthorizedError();
         }
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+        const user = await UserModel.findById(userData.id)
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateToken({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return { ...tokens, user: userDto };
     }
 }
 
