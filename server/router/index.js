dca562ac3ecc15a0b9bacc5a84c0a2640d498f71
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-03 19:50:08 +0200

    server: + validation email & pass on registration

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,15 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user.controller.js');
+const { body } = require('express-validator');
 
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post(
+    '/registration',
+    body('email').isEmail(),
+    body('password').isLength({ min: 3, max: 32 }),
+    userController.registration
+);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
