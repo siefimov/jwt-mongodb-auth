35ec1b88f29200516ec813ff4c16bac65758f3dd
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-08 00:20:47 +0200

    client: * store and main.tsx

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './auth/authSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        users: authSlice,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
