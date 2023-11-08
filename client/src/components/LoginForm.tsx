06c880664b26ca84fa7c480616235b364df31140
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-08 16:39:56 +0200

    client: * LoginForm component

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,15 +1,24 @@
 import { FC, useState } from 'react';
+import { useAppDispatch } from '../store/hooks';
+import { login, registration } from '../store/auth/authThunks';
 
 export const LoginForm: FC = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
+    const dispatch = useAppDispatch();
+
     return (
         <div>
             <input value={email} onChange={(e) => setEmail(e.target.value)} type='text' placeholder='Email' />
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type='text' placeholder='Password' />
-            <button>Login</button>
-            <button>Registration</button>
+            <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type='password'
+                placeholder='Password'
+            />
+            <button onClick={() => dispatch(login({ email, password }))}>Login</button>
+            <button onClick={() => dispatch(registration({ email, password }))}>Registration</button>
         </div>
     );
 };
