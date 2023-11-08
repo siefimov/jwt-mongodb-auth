cc1eba1c83498ea0df0b2b9f1f0b5643d38aa821
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-11-08 16:40:21 +0200

    client: * App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,51 @@
-import {FC} from 'react'
+import { FC, useEffect, useState } from 'react';
+import { LoginForm } from './components/LoginForm';
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { checkAuth, logout } from './store/auth/authThunks';
+import { IUser } from './models/IUser';
+import UserService from './services/UserService';
 
 const App: FC = () => {
+    const user = useAppSelector((state) => state.users);
+    const [users, setUsers] = useState<IUser[]>([]);
+    const dispatch = useAppDispatch();
 
-  return (
-    <div>
-      test
-    </div>
-  )
-}
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            dispatch(checkAuth());
+        }
+    }, [dispatch]);
 
-export default App
+    const getUsers = async () => {
+        try {
+            const response = await UserService.fetchUsers();
+            setUsers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    if (user.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!user.isAuth) {
+        return <LoginForm />;
+    }
+
+    return (
+        <div>
+            <h1>{user.isAuth ? `User is authorized ${user.user.email}` : 'Log In'}</h1>
+            <h1>{user.user.isActivated ? 'Account is activated' : 'Comfirm your account'}</h1>
+            <button onClick={() => dispatch(logout())}>Log Out</button>
+            <div>
+                <button onClick={getUsers}>The list of users</button>
+                {users.map((user) => (
+                    <div key={user.email}>{user.email}</div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
